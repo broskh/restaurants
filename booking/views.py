058a83b135ad3547fd60b9bae3231e956c1a09aa
broskh <alessio.scheri@gmail.com>
2018-06-14 16:53:42 +0200

    Correctly set static files and test to show a simple page.

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
-def index (request):
-    return HttpResponse("Hello world")
+def index(request):
+    template = loader.get_template('booking/index.html')
+    return HttpResponse(template.render({}, request))
